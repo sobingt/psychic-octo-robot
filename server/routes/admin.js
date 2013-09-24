1fd5e07b7c5974babf0a7e7e6fa2ec47f3d92072
Sobin George Thomas <sobingt@gmail.com>
2013-09-24 18:12:09 +0530

    Added Forms

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,3 +6,9 @@
 exports.index = function(req, res){
   res.render('admin/index', { title: 'Express' });
 };
+exports.createuser = function(req, res){
+  res.render('admin/createuser', { title: 'Express' });
+};
+exports.createmeal = function(req, res){
+  res.render('admin/createmeal', { title: 'Express' });
+};
